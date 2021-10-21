12f2f2067ce846278afeadb320d2b9f6eeccee12
Daehan Kim <really@iamghost.kr>
2021-10-21 11:03:14 +0900

    Fix indents

--- ios/VisionCameraFaceDetector.swift
+++ ios/VisionCameraFaceDetector.swift
@@ -8,58 +8,55 @@ import AVFoundation
 
 @objc(VisionCameraFaceDetector)
 public class VisionCameraFaceDetector: NSObject, FrameProcessorPluginBase {
-
-
-  static var FaceDetectorOption: FaceDetectorOptions = {
-    let option = FaceDetectorOptions()
-    option.contourMode = .all
-    option.classificationMode = .all
-    option.landmarkMode = .all
-    option.performanceMode = .accurate // doesn't work in fast mode!, why?
-    return option
-  }()
-
-  static var faceDetector = FaceDetector.faceDetector(options: FaceDetectorOption)
-  
+    static var FaceDetectorOption: FaceDetectorOptions = {
+        let option = FaceDetectorOptions()
+        option.contourMode = .all
+        option.classificationMode = .all
+        option.landmarkMode = .all
+        option.performanceMode = .accurate // doesn't work in fast mode!, why?
+        return option
+    }()
+    
+    static var faceDetector = FaceDetector.faceDetector(options: FaceDetectorOption)
+    
     private static func processContours(from faces: [Face]?) -> [String:CGFloat] {
-    // TODO: implement face contour calculations
-    return [:]
-  }
+        // TODO: implement face contour calculations
+        return [:]
+    }
     
     private static func processBoundingBox(from faces: [Face]?) -> [String:CGFloat] {
-    // TODO: implement bounding box calculations
-    return [:]
-  }
+        // TODO: implement bounding box calculations
+        return [:]
+    }
     
-  @objc
-  public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
-    
-    let image = VisionImage(buffer: frame.buffer)
-    image.orientation = .up
-
-    var faceAttributes: [Any] = []
-    
-    do {
-       let faces: [Face] =  try faceDetector.results(in: image)
-       if (!faces.isEmpty){
-            for face in faces {
-               var map: [String: Any] = [:]
-               
-                map["rollAngle"] = face.headEulerAngleZ  // Head is tilted sideways rotZ degrees
-                map["pitchAngle"] = face.headEulerAngleX  // Head is rotated to the uptoward rotX degrees
-                map["yawAngle"] = face.headEulerAngleY   // Head is rotated to the right rotY degrees
-                map["leftEyeOpenProbability"] = face.leftEyeOpenProbability
-                map["rightEyeOpenProbability"] = face.rightEyeOpenProbability
-                map["smilingProbability"] = face.smilingProbability
-                map["bounds"] = processBoundingBox(from: faces)
-                map["cotours"] = processContours(from: faces)
-
-                faceAttributes.append(map)
+    @objc
+    public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
+        let image = VisionImage(buffer: frame.buffer)
+        image.orientation = .up
+        
+        var faceAttributes: [Any] = []
+        
+        do {
+            let faces: [Face] =  try faceDetector.results(in: image)
+            if (!faces.isEmpty){
+                for face in faces {
+                    var map: [String: Any] = [:]
+                    
+                    map["rollAngle"] = face.headEulerAngleZ  // Head is tilted sideways rotZ degrees
+                    map["pitchAngle"] = face.headEulerAngleX  // Head is rotated to the uptoward rotX degrees
+                    map["yawAngle"] = face.headEulerAngleY   // Head is rotated to the right rotY degrees
+                    map["leftEyeOpenProbability"] = face.leftEyeOpenProbability
+                    map["rightEyeOpenProbability"] = face.rightEyeOpenProbability
+                    map["smilingProbability"] = face.smilingProbability
+                    map["bounds"] = processBoundingBox(from: faces)
+                    map["cotours"] = processContours(from: faces)
+                    
+                    faceAttributes.append(map)
+                }
             }
-       }
-       } catch _ {
-           return nil
-       }
-    return faceAttributes
-  }
+        } catch _ {
+            return nil
+        }
+        return faceAttributes
+    }
 }
